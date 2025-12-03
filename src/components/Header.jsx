6c2df6b6fac777b8e84377b3804d7f8258e8f4df
Ruzaki075 <chernevigor78@gmail.com>
2025-12-04 00:05:49 +0300

    "header"

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../styles/Header.css"
+
+function Header(){
+    return(
+        <div className="wrapper">
+            <ul>
+                <li>Ruzakkki</li>
+                <li>Frontend-developer</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
